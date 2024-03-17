c9b3f56c1f0f3f7a1ceba9791aa96e761ca31a12
msm-faraj <farajzademeysam@gmail.com>
2024-03-17 20:37:07 +0100

    ac account and category to transaction form

--- src/components/transaction/TransactionForm.tsx
+++ src/components/transaction/TransactionForm.tsx
@@ -8,6 +8,7 @@ import {
   StackDivider,
   VStack,
   Heading,
+  Select,
 } from "@chakra-ui/react";
 
 import { z } from "zod";
@@ -16,6 +17,8 @@ import { useForm } from "react-hook-form";
 import axios from "../../api/axios";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
+import accounts from "../../data/accounts";
+import categories from "../../data/categories";
 
 const TRANSACTION_URL = "/api/transactions";
 
@@ -29,8 +32,8 @@ const schema = z.object({
   date: z.string(),
   note: z.string().min(3).max(50),
   amount: z.number().min(0.01).max(100_000),
-  // account: z.string().min(3).max(50),
-  // category: z.string(),
+  account: z.string().min(3).max(50),
+  category: z.string(),
   description: z
     .string()
     .min(3, { message: "Description must be at least 3 char" })
@@ -55,11 +58,21 @@ export const TransactionForm = ({ forceUpdate }: Props) => {
 
   const onSubmit = async (e: EpxenseFormData) => {
     try {
-      await axios.post(TRANSACTION_URL, e, {
-        headers: {
-          "x-auth-token": auth.accessToken,
+      await axios.post(
+        TRANSACTION_URL,
+        {
+          amount: e.amount,
+          note: e.note,
+          description: e.description,
+          date: e.date,
+          type: e.type,
         },
-      });
+        {
+          headers: {
+            "x-auth-token": auth.accessToken,
+          },
+        }
+      );
       // console.log(response.data);
       // setNewTransaction(response.data);
       forceUpdate();
@@ -69,10 +82,12 @@ export const TransactionForm = ({ forceUpdate }: Props) => {
   };
 
   return (
-    <Box boxShadow={"dark-lg"} p={5} borderRadius={5} m={2} w={"50%"}>
-      <Heading as={"h2"} size={"md"} pb={4}>
-        Transaction Form
-      </Heading>
+    <Box boxShadow={"dark-lg"} p={5} borderRadius={5} m={2} w={"90%"}>
+      <VStack align="stretch">
+        <Heading as={"h2"} size={"md"} pb={4}>
+          Transaction Form
+        </Heading>
+      </VStack>
       <form
         onSubmit={handleSubmit((e) => {
           onSubmit(e);
@@ -80,132 +95,108 @@ export const TransactionForm = ({ forceUpdate }: Props) => {
         })}
       >
         <VStack divider={<StackDivider />} spacing={1} align="stretch">
-          {/* Date */}
-          <HStack>
-            <Box>
-              <FormLabel htmlFor="date">date</FormLabel>
-            </Box>
-            <Box>
-              <Input {...register("date")} id="date" type="text"></Input>
-              {errors.date && (
-                <Text fontSize={style.errorFontSize} color={style.colorDanger}>
-                  {errors.date.message}
-                </Text>
-              )}
-            </Box>
-          </HStack>
-
           {/* Account */}
-          {/* <HStack>
-            <Box>
-              <FormLabel htmlFor="account">account</FormLabel>
-            </Box>
-            <Box>
-              <Select {...register("account")} id="account">
-                <option>Select Account</option>
-                {accounts.map((account) => (
-                  <option key={account.id} value={account.name}>
-                    {account.name}
-                  </option>
-                ))}
-              </Select>
-              {errors.account && (
-                <Text fontSize={style.errorFontSize} color={style.colorDanger}>
-                  {errors.account.message}
-                </Text>
-              )}
-            </Box>
-          </HStack> */}
+          <HStack>
+            <FormLabel htmlFor="account">account</FormLabel>
+            <Select {...register("account")} id="account">
+              {/* <option>Select Account</option> */}
+              {accounts.map((account) => (
+                <option key={account.id} value={account.name}>
+                  {account.name}
+                </option>
+              ))}
+            </Select>
+          </HStack>
+          {errors.account && (
+            <Text fontSize={style.errorFontSize} color={style.colorDanger}>
+              {errors.account.message}
+            </Text>
+          )}
 
           {/* Category */}
-          {/* <HStack>
-            <Box>
-              <FormLabel htmlFor="category">category</FormLabel>
-            </Box>
-            <Box>
-              <Select {...register("category")} id="category">
-                <option>Select Category</option>
-                {categories.map((category) => (
-                  <option key={category.id} value={category.name}>
-                    {category.name}
-                  </option>
-                ))}
-              </Select>
-              {errors.category && (
-                <Text fontSize={style.errorFontSize} color={style.colorDanger}>
-                  {errors.category.message}
-                </Text>
-              )}
-            </Box>
-          </HStack> */}
-
-          {/* Amount */}
           <HStack>
-            <Box>
-              <FormLabel htmlFor="amount">amount</FormLabel>
-            </Box>
-            <Box>
-              <Input
-                {...register("amount", { valueAsNumber: true })}
-                id="amount"
-                type="text"
-              ></Input>
-              {errors.amount && (
-                <Text fontSize={style.errorFontSize} color={style.colorDanger}>
-                  {errors.amount.message}
-                </Text>
-              )}
-            </Box>
+            <FormLabel htmlFor="category">category</FormLabel>
+            <Select {...register("category")} id="category">
+              {/* <option>Select Category</option> */}
+              {categories.map((category) => (
+                <option key={category.id} value={category.name}>
+                  {category.name}
+                </option>
+              ))}
+            </Select>
           </HStack>
-
-          {/* Note */}
-          <HStack>
-            <Box>
-              <FormLabel htmlFor="note">note</FormLabel>
-            </Box>
-            <Box>
-              <Input {...register("note")} id="note" type="text"></Input>
-              {errors.note && (
-                <Text fontSize={style.errorFontSize} color={style.colorDanger}>
-                  {errors.note.message}
-                </Text>
-              )}
-            </Box>
-          </HStack>
+          {errors.category && (
+            <Text fontSize={style.errorFontSize} color={style.colorDanger}>
+              {errors.category.message}
+            </Text>
+          )}
 
           {/* Type */}
           <HStack>
-            <Box>
-              <FormLabel htmlFor="type">type</FormLabel>
-            </Box>
-            <Box>
-              <Input {...register("type")} id="type" type="text"></Input>
-              {errors.type && (
-                <Text fontSize={style.errorFontSize} color={style.colorDanger}>
-                  {errors.type.message}
-                </Text>
-              )}
-            </Box>
+            <FormLabel htmlFor="type">type</FormLabel>
+            <Select {...register("type")} id="type">
+              {/* <option>Select Type</option> */}
+              <option>expense</option>
+              <option>inocme</option>
+            </Select>
           </HStack>
+          {errors.type && (
+            <Text fontSize={style.errorFontSize} color={style.colorDanger}>
+              {errors.type.message}
+            </Text>
+          )}
+
+          {/* Date */}
+          <HStack>
+            <FormLabel htmlFor="date">date</FormLabel>
+            <Input {...register("date")} id="date" type="text"></Input>
+          </HStack>
+          {errors.date && (
+            <Text fontSize={style.errorFontSize} color={style.colorDanger}>
+              {errors.date.message}
+            </Text>
+          )}
+
+          {/* Amount */}
+          <HStack>
+            <FormLabel htmlFor="amount">amount</FormLabel>
+            <Input
+              {...register("amount", { valueAsNumber: true })}
+              id="amount"
+              type="text"
+            ></Input>
+          </HStack>
+          {errors.amount && (
+            <Text fontSize={style.errorFontSize} color={style.colorDanger}>
+              {errors.amount.message}
+            </Text>
+          )}
+
+          {/* Note */}
+          <HStack>
+            <FormLabel htmlFor="note">note</FormLabel>
+            <Input {...register("note")} id="note" type="text"></Input>
+          </HStack>
+          {errors.note && (
+            <Text fontSize={style.errorFontSize} color={style.colorDanger}>
+              {errors.note.message}
+            </Text>
+          )}
 
           {/* Description */}
           <HStack>
-            <Box>
-              <FormLabel htmlFor="description">descripton</FormLabel>
-            </Box>
-            <Box>
-              <Input
-                {...register("description")}
-                id="description"
-                type="text"
-              ></Input>
-              {errors.description && (
-                <Text fontSize={style.errorFontSize} color={style.colorDanger}>
-                  {errors.description.message}
-                </Text>
-              )}
-            </Box>
+            <FormLabel htmlFor="description">descripton</FormLabel>
+            <Input
+              {...register("description")}
+              id="description"
+              type="text"
+            ></Input>
           </HStack>
+          {errors.description && (
+            <Text fontSize={style.errorFontSize} color={style.colorDanger}>
+              {errors.description.message}
+            </Text>
+          )}
           <Button isDisabled={!isValid} type="submit">
             Send
           </Button>
