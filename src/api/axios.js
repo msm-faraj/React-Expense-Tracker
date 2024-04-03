5d1374dd0a22bd7008260e0b7731cacef3bc5702
msm-faraj <farajzademeysam@gmail.com>
2024-04-03 03:42:52 +0200

    change base url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 // const BASE_URL = "http://localhost:3000";
-const BASE_URL = "https://my-expense-tracker-8e533260d61a.herokuapp.com";
+const BASE_URL =
+  "https://msm-transaction-tracker-api-1fcf9cf151aa.herokuapp.com/";
 
 export default axios.create({
   baseURL: BASE_URL,
